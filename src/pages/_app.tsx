78fe4cc8822510e68b728cf9f9c2d0c6c8806ed9
joaoeduardodias <joaoeduardodias123@gmail.com>
2022-02-21 14:06:47 -0300

    feat: create component slide

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+/* eslint-disable import/no-unresolved */
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { theme } from '../styles/theme';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import '../styles/slide.css';
 
 const MyApp = function ({ Component, pageProps }: AppProps): JSX.Element {
   return (
